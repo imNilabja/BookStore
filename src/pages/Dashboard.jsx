a1c83d20b4e158e0026aa3359b54660e2ed3be99
iamNilabja <goswamineel23.com>
2024-12-12 20:32:53 +0530

    dashboard modified

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,13 +2,27 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import CarouselFade from '../components/CarousalFade'
+import { useState } from 'react'
 const Dashboard = () => {
+
+  const [status, setstatus] = useState(localStorage.getItem('login') === 'true')
   return (
     <>
-     <Navbar/>
-     <CarouselFade/>
+      <Navbar />
+      <CarouselFade />
+      {status ? (
+        <>
+          <p className='text-2xl font-bold'>
+            Login to Access!
+          </p></>
+      ) : (
+        <>
+          <p className='text-2xl font-bold'>
+            you have Access!
+          </p></>
+      )}
     </>
-   
+
   )
 }
 
