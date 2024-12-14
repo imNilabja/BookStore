cf50757cafe522ac1cbbaad73e4a84e3e90de2fc
iamNilabja <goswamineel23.com>
2024-12-15 00:20:04 +0530

    price set

--- src/components/BookContainer.jsx
+++ src/components/BookContainer.jsx
@@ -6,9 +6,9 @@ const BookContainer = () => {
   return (
     <>
     <div className='w-[80%] min-h-[50%] max-h-fit border-4 border-blue-900  mx-auto my-8 rounded-md p-5 flex flex-wrap gap-10 items-center justify-center'>
-        <BookCards link="cloudy.png" name="Papbiddha" price="250"/>
+        <BookCards link="cloudy.png" name="Naruto" price="299"/>
         <BookCards link="evening.png" name="Papbiddha" price="250"/>
-        <BookCards link="rainy.png" name="Papbiddha" price="250"/>
+        <BookCards link="rainy.png" name="JAVA Guide" price="450"/>
        
       
     </div>
