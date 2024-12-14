1e1cf76791283564377f2861fcef16617fa9b368
iamNilabja <goswamineel23.com>
2024-12-14 23:32:41 +0530

    SD

--- src/components/BookContainer.jsx
+++ src/components/BookContainer.jsx
@@ -7,14 +7,7 @@ const BookContainer = () => {
     <>
     <div className='w-[80%] min-h-[50%] max-h-fit border-4 border-blue-900  mx-auto my-8 rounded-md p-5 flex flex-wrap gap-10 items-center justify-center'>
         <BookCards link="cloudy.png" name="Papbiddha" price="250"/>
-        <BookCards link="cloudy.png" name="Papbiddha" price="250"/>
         <BookCards link="evening.png" name="Papbiddha" price="250"/>
-        <BookCards link="cloudy.png" name="Papbiddha" price="250"/>
-        <BookCards link="cloudy.png" name="Papbiddha" price="250"/>
-        <BookCards link="evening.png" name="Papbiddha" price="250"/>
-        <BookCards link="evening.png" name="Papbiddha" price="250"/>
-        <BookCards link="rainy.png" name="Papbiddha" price="250"/>
-        <BookCards link="rainy.png" name="Papbiddha" price="250"/>
         <BookCards link="rainy.png" name="Papbiddha" price="250"/>
        
       
