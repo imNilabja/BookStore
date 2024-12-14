03f4da32e70463f2a935266dca182144c5773ddc
iamNilabja <goswamineel23.com>
2024-12-14 23:39:36 +0530

    img mod

--- src/components/BookCards.jsx
+++ src/components/BookCards.jsx
@@ -8,7 +8,7 @@ const BookCards = ({ link,name,price }) => {
             <div className='w-[220px] h-[350px] border border-black rounded-md flex-col items-center justify-center'>
                 <div className=" w-[95%] h-[175px] my-auto mx-auto p-1">
 
-                    <img src={link} alt="" />
+                    <img src={link} alt="" className='rounded-md'/>
 
                 </div>
                 <div className=" w-[95%] h-[175px] my-auto mx-auto p-2 flex-col justify-center items-center font-bold">
